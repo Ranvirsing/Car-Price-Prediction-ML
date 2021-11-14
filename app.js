f16293ba72eab8cf95124f290a27bf0d8b610679
Ranvirsing <81947226+Ranvirsing@users.noreply.github.com>
2021-11-15 00:27:40 +0530

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ function onClickedEstimatePrice() {
   //var url = "http://127.0.0.1:5000/get_est_price"; //Use this if you are NOT using nginx which is first 7 tutorials
   var url = "https://carpredml.herokuapp.com/predict_home_price"; // Use this if  you are using nginx. i.e tutorial 8 and onwards
   const port = Process.env.PORT || 3000 ;
-  server.listen(port)
+  $.listen(port)
   $.post(port/get_est_price, {
       year: parseFloat(year_.value),
       fuel: fuel_.value,
