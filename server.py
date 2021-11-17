9d9fadfaef883ba12e67524494f78a608c6e768a
Ranvirsing <81947226+Ranvirsing@users.noreply.github.com>
2021-11-17 13:12:10 +0530

    Update server.py

--- server.py
+++ server.py
@@ -4,7 +4,7 @@ app=Flask(__name__)
 @app.route('/')
 def apps():
     return render_template('app.html')
-@app.route('/get_est_price', methods = ['POST','GET'])
+@app.route('/get_est_price', methods = ['POST'])
 def get_est_price():
     year=int(request.form['year'])
     km=int(request.form['km'])
